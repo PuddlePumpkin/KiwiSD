bdbe2278cb1f0e3037493cb14fc82f3a07bbb5b5
KeiraTheCat <66428305+PuddlePumpkin@users.noreply.github.com>
2022-11-20 20:14:27 -0600

    change %cd% for %~dp0

--- kiwi.bat
+++ kiwi.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=%CD%\venv
+set VIRTUAL_ENV=%~dp0\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -31,4 +31,4 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" %_OLD_CODEPAGE% > nul
     set _OLD_CODEPAGE=
 )
-python "%CD%\kiwi.py"
+python "%~dp0\kiwi.py"
