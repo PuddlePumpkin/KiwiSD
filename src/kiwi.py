72f4e4134c10485e75082bfa65614e8895af4459
KeiraTheCat <66428305+PuddlePumpkin@users.noreply.github.com>
2022-11-28 16:15:37 -0600

    fix change model announce [0] error

--- src/kiwi.py
+++ src/kiwi.py
@@ -1030,12 +1030,15 @@ async def ThreadCompletionLoop():
         for channel in config["ChangeModelChannelsToNotify"].replace(", ",",").split(","):
             if channel != str(AwaitingModelChangeContext.channel_id):
                 messages = await bot.rest.fetch_messages(int(channel)).take_until(lambda m: datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=1) > m.created_at).limit(1)
-                if messages[0].author.id == bot.get_me().id:
-                    if messages[0].embeds[0].title.startswith("Loaded"):
-                        await bot.rest.edit_message(int(channel),messages[0].id,embed)
+                try:
+                    if messages[0].author.id == bot.get_me().id:
+                        if messages[0].embeds[0].title.startswith("Loaded"):
+                            await bot.rest.edit_message(int(channel),messages[0].id,embed)
+                        else:
+                            await bot.rest.create_message(int(channel),embed)
                     else:
                         await bot.rest.create_message(int(channel),embed)
-                else:
+                except:
                     await bot.rest.create_message(int(channel),embed)
         botBusy = False
         AwaitingModelChangeContext = None
